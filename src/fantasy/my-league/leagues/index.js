7e8cca25b9070e698e2200fe567b56a69e5e6255
chloedipetrillo <dipetrillo.c@northeastern.edu>
2023-04-21 11:32:40 -0400

    fixed leagues page to only be visible to logged in users

--- src/fantasy/my-league/leagues/index.js
+++ src/fantasy/my-league/leagues/index.js
@@ -1,16 +1,40 @@
 import SearchLeaguesComponent from "./search-leagues-component";
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+import {profileThunk} from "../../../services/users/users-thunks";
 
-function LeaguesComponent() {
+const LeaguesComponent = () => {
+
+    const dispatch = useDispatch();
+    const { currentUser, load} = useSelector((state) => state.users);
+    const [profile, setProfile] = useState(currentUser);
+    const getUser = async () =>{
+        const { payload } = await dispatch(profileThunk());
+        setProfile(payload);
+        return payload
+    }
+    useEffect( () => {
+        getUser();
+    }, []);
     return (
-        <div className="row mt-2">
-            <div className="d-12 col-md-6">
-                SEARCH FUNCITONALITY TO SEARCH ALL  LEAGUES (WILL HAVE NAME CANT CLICK UNLESS U JOIN IT)
-                <SearchLeaguesComponent/>
+
+    <>
+        {profile && (
+            <div className="row mt-2">
+                <div className="d-12 col-md-6">
+                    SEARCH FUNCITONALITY TO SEARCH ALL  LEAGUES (WILL HAVE NAME CANT CLICK UNLESS U JOIN IT)
+                    <SearchLeaguesComponent/>
+                </div>
+                <div className="col-12 col-md-6">
+                    LIST OF ALL YOUR LEAGUES IF YOU CLICK ONE IT WILL LINK TO THE LEADERBOARD
+                </div>
             </div>
-            <div className="col-12 col-md-6">
-                LIST OF ALL YOUR LEAGUES IF YOU CLICK ONE IT WILL LINK TO THE LEADERBOARD
-            </div>
-        </div>
+        )}
+
+    </>
+
+
+
     );
 }
 
