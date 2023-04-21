ea84269a65943c9dc6ff23e70332eb189e426ce5
gujralajit <gujralajit@gmail.com>
2023-04-21 15:57:31 -0400

    Added functionality for admin to reset scores.

--- src/fantasy/admin-page/manage-scores.js
+++ src/fantasy/admin-page/manage-scores.js
@@ -26,21 +26,24 @@ function ManageScoresComponent() {
     const setScores = async () => {
 
         for (const player of usersData) {
-            console.log("the user is : " + player._id);
-            // let team = findTeamThunk()
-            // console.log("the team is : " + team);
             let num = (randomNumberInRange(-5, 50));
-            console.log("score for the user is : " + num);
             const t = await findTeams(player._id);
             if (t != null) {
-                console.log("team here is: " + JSON.stringify(t))
-                // alert("Updating your drafted team!")
-                // updateTeam(teamEntry)
-                // console.log("totalscores are : " + t.totalScores)
                 let total_scores = parseInt(t.totalScores) + num
-                console.log("updated scores are : " + total_scores)
                 const updatedEntry = {...t, scoresThisWeek: num, totalScores: total_scores}
                 console.log("updated entry is : " + JSON.stringify(updatedEntry))
+                dispatch(updateTeamThunk(updatedEntry))
+            }
+        }
+    }
+    const resetScores = async () => {
+        console.log("resetting all the scores to 0s! ")
+        for (const player of usersData) {
+            let num = 0
+            const t = await findTeams(player._id);
+            if (t != null) {
+                let total_scores = 0
+                const updatedEntry = {...t, scoresThisWeek: num, totalScores: total_scores}
                 dispatch(updateTeamThunk(updatedEntry))
             }
         }
@@ -49,16 +52,20 @@ function ManageScoresComponent() {
         setScores();
     };
 
+    const handleResetClick = () => {
+        resetScores();
+    };
+
     return (
 
         <div className="center mt-2">
-           Ajit to put stuff here
             <div >
-                <button className="btn btn-danger" onClick={handleClick}>
+                <button className="btn btn-danger me-2" onClick={handleResetClick}>
+                    Reset Scores
+                </button>
+                <button className="btn btn-success" onClick={handleClick}>
                     Update Scores
                 </button>
-                {/*<h2>number is: {num}</h2>*/}
-                {/*<h3>users are : {JSON.stringify(usersData)}</h3>*/}
             </div>
         </div>
 
