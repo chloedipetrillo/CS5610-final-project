0af34e2f80bd1e34ac5f1f77025cc8da24ffea1e
gujralajit <gujralajit@gmail.com>
2023-04-21 14:47:51 -0400

    Added scoring functionality to the admin page.

--- src/fantasy/admin-page/manage-scores.js
+++ src/fantasy/admin-page/manage-scores.js
@@ -1,21 +1,64 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {findAllCommentsThunk} from "../../services/comments/comment-thunks";
 import {findAllUsersThunk} from "../../services/users/users-thunks";
-import {findAllUsers} from "../../services/users/users-services";
-import IndividualComponent from "./individuals";
-
+import {findTeams} from "../../services/my-team/my-team-services";
+import {updateTeamThunk} from "../../services/my-team/my-team-thunks";
 
 function ManageScoresComponent() {
+
+    const {usersData} = useSelector(state => state.users)
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(findAllUsersThunk());
+    }, [])
+
+    // usersData.forEach(function(player) {
+    //     console.log("the user is : " + player.username);
+    // });
+    // const [num, setNum] = useState(0);
+    // const [team, setTeam] = useState();
+    function randomNumberInRange(min, max) {
+        // get number between min (inclusive) and max (inclusive)
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    const setScores = async () => {
+
+        for (const player of usersData) {
+            console.log("the user is : " + player._id);
+            // let team = findTeamThunk()
+            // console.log("the team is : " + team);
+            let num = (randomNumberInRange(-5, 50));
+            console.log("score for the user is : " + num);
+            const t = await findTeams(player._id);
+            if (t != null) {
+                console.log("team here is: " + JSON.stringify(t))
+                // alert("Updating your drafted team!")
+                // updateTeam(teamEntry)
+                // console.log("totalscores are : " + t.totalScores)
+                let total_scores = parseInt(t.totalScores) + num
+                console.log("updated scores are : " + total_scores)
+                const updatedEntry = {...t, scoresThisWeek: num, totalScores: total_scores}
+                console.log("updated entry is : " + JSON.stringify(updatedEntry))
+                dispatch(updateTeamThunk(updatedEntry))
+            }
+        }
+    }
+    const handleClick = () => {
+        setScores();
+    };
 
     return (
 
         <div className="center mt-2">
            Ajit to put stuff here
             <div >
-                <button className="btn btn-danger">
+                <button className="btn btn-danger" onClick={handleClick}>
                     Update Scores
                 </button>
+                {/*<h2>number is: {num}</h2>*/}
+                {/*<h3>users are : {JSON.stringify(usersData)}</h3>*/}
             </div>
         </div>
 
